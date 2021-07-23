341c6ba0874d918d4a3a19df9c64cc0b5ab39516
Simon Séhier <simon.sehier@camptocamp.com>
2021-07-23 16:14:58 +0200

    add output cluster_id

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output "kubeconfig" {
   value       = local.kubeconfig
   sensitive   = true
 }
+
+output "cluster_id" {
+  description = "this cluster id"
+  value       = opentelekomcloud_cce_cluster_v3.cluster.cluster_id
+}
